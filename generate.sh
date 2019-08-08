af9101d5d5ad26139129f3f368c6c2e4b6b9bf16
Darren Cheng <darren@thanx.com>
2019-08-08 15:02:15 -0700

    Initial Swagger -> API documentation logic.

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Converts Swagger JSON in /swagger into HTML in /docs
+
+cd swagger
+
+for file in *.json; do
+  if [[ -f $file ]]; then
+    name=${file//.json/}
+    echo "Generating '$name' API docs"
+    spectacle $file -f $name.html
+    mv public/$name.html ../docs
+    rm -rf public
+  fi
+done
